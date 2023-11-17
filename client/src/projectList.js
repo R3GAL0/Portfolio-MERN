71d358b390b3bf20e590256636d9d9d362f6f2d1
R3GAL0 <137714605+R3GAL0@users.noreply.github.com>
2023-11-17 16:26:56 -0500

    adding to about me and projects listed

--- client/src/projectList.js
+++ client/src/projectList.js
@@ -2,10 +2,18 @@
 // list of projects to appear on the main page
 // 0: key, 1: github link, 2: deploymeny/video link, 3: img ref from 'public/projectImgs', 4: project title
 let projs = [
+    
+    [
+        'p6', 
+        'https://github.com/R3GAL0/Tech-Blog', 
+        'https://tech-blog12534-b3073f956948.herokuapp.com/', 
+        '/projectImgs/blog.png', 
+        'Tech Blog'
+    ],
     [
         'p1',
-        'https://github.com/phvania/E-Commerce-',
-        'https://riley1-3f2201f3866a.herokuapp.com/',
+        'https://github.com/R3GAL0/project03-solo',
+        'https://e-store-e55ee7daa6ad.herokuapp.com/',
         '/projectImgs/shoppingBag.jpg',
         'E-Commerce'
     ],
@@ -32,18 +40,11 @@ let projs = [
     ], 
     [
         'p3', 
-        'https://github.com/R3GAL0/Project01', 
-        'https://r3gal0.github.io/Project01/', 
+        'https://github.com/R3GAL0/project01-solo', 
+        'https://main--tourmaline-klepon-57d20b.netlify.app/', 
         '/projectImgs/pngtree-food-logo-png-image_8239850.png', 
         "Restaurant Locator"
     ],
-    // [
-    //     'p6', 
-    //     '', 
-    //     '', 
-    //     '/projectImgs/cat.jpg', 
-    //     ''
-    // ],
 
 ]
 
