5f2f3bae7c087b22089af63172b1c11e73a6e5ef
R3GAL0 <137714605+R3GAL0@users.noreply.github.com>
2023-11-22 13:47:52 -0500

    changing available projects

--- client/src/projectList.js
+++ client/src/projectList.js
@@ -1,23 +1,26 @@
 
 // list of projects to appear on the main page
-// 0: key, 1: github link, 2: deploymeny/video link, 3: img ref from 'public/projectImgs', 4: project title
+// 0: key, 1: github link, 2: deployment/video link, 
+// 3: img ref from 'public/projectImgs', 4: project title
 let projs = [
-    
     [
-        'p6', 
-        'https://github.com/R3GAL0/Tech-Blog', 
-        'https://tech-blog12534-b3073f956948.herokuapp.com/', 
-        '/projectImgs/blog.png', 
-        'Tech Blog'
-    ],
-    [
+        // project 3
         'p1',
         'https://github.com/R3GAL0/project03-solo',
         'https://e-store123-c8f23fba8782.herokuapp.com/',
         '/projectImgs/shoppingBag.jpg',
         'E-Commerce'
+    ],    
+    [
+        // project 2
+        'p7',
+        'https://github.com/R3GAL0/project02-solo',
+        'https://floating-sands-48118-d38c73d975f6.herokuapp.com/',
+        '/projectImgs/medInfo.png',
+        'Med-Info'
     ],
     [
+        // challenge 18
         'p2',
         'https://github.com/R3GAL0/Social-Network', 
         'https://mega.nz/file/CggHCAAD#x99X8-23wPnOnik6gji5HHFoC4sliLPZ-oEYJg5uoSY', 
@@ -25,26 +28,31 @@ let projs = [
         'Social Network'
     ],
     [
+        // challenge 5
         'p4', 
         'https://github.com/R3GAL0/Work-day-scheduler', 
         'https://r3gal0.github.io/Work-day-scheduler/', 
         '/projectImgs/timetable.jpg', 
         'Work Day Schedueler'
     ], 
-    [
-        'p5', 
-        'https://github.com/R3GAL0/JAST', 
-        'https://jast1231-f873e0028cf0.herokuapp.com/', 
-        '/projectImgs/notebook.jpg', 
-        'Text Editor'
-    ], 
-    [
-        'p7',
-        'https://github.com/R3GAL0/project02-solo',
-        'https://floating-sands-48118-d38c73d975f6.herokuapp.com/',
-        '/projectImgs/medInfo.png',
-        'Med-Info'
-    ],
+    // deployments have been spun down to save on heroku site credits
+    // in the process of redeploying to netlify
+    // [
+    //     // challenge 19
+    //     'p5', 
+    //     'https://github.com/R3GAL0/JAST', 
+    //     'https://jast1231-f873e0028cf0.herokuapp.com/', 
+    //     '/projectImgs/notebook.jpg', 
+    //     'Text Editor'
+    // ], 
+    // [
+    //     // challenge 14
+    //     'p6', 
+    //     'https://github.com/R3GAL0/Tech-Blog', 
+    //     'https://tech-blog12534-b3073f956948.herokuapp.com/', 
+    //     '/projectImgs/blog.png', 
+    //     'Tech Blog'
+    // ],
     // hidden until i can enable the api key
     // [
     //     'p3', 
@@ -55,8 +63,28 @@ let projs = [
     // ],
 
 ]
-// other functional deployments
+// other functional deployments (they have been spun down)
 // https://fast-earth-38465-d294b04d7e69.herokuapp.com/
+// UI/UX Tips
 // https://agile-basin-69547-9c338cf470d4.herokuapp.com/
 
 export default projs
+// challenges
+
+// challenge 6 - weather dashboard
+// https://r3gal0.github.io/Weather-Dashboard/
+// 7+8/10 - readme generator
+// 11 - note taker
+// https://agile-basin-69547-9c338cf470d4.herokuapp.com/
+// 12 - employee tracker
+// video link
+// 13 - e-commerce backend
+// video link
+// 14 - tech blog
+// (complex)
+// 17 - regex tutorial
+// 18 - social network api
+// video link
+// 19 - pwa text editor (JAST)
+// 20 - react portfolio
+// 21 - MERN book search
